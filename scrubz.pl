8bffaabd3f19a30e55626a454a9077f74d5eab2c
Tony Tosi <ttosi519@gmail.com>
2016-12-05 10:39:55 -0800

    add average file processing time

--- scrubz.pl
+++ scrubz.pl
@@ -3,13 +3,13 @@ use strict;
 use warnings;
 use threads;
 use threads::shared;
-#use Digest::MD5 qw(md5_hex);
 use Digest::SHA qw(sha512_hex);
 
 
 my $numThreads = 4; # how many files to be processed in parallel (use number of cores)
 my $recordBufferSize = 10000; # how many records to be written out at once
-my $seed = "958102fc79354a718b1ee220498b153c";
+my $seed = "958102fc79354a718b1ee220498b153cc058da7699634fb1a927ab65d031c45c";
+my @fileTimes;
 
 my $soureDir = "source";
 my $processedDir = "processed";
@@ -36,7 +36,7 @@ sub Process_File {
 	while(scalar(@files) != 0) {
 		my $inFile = pop(@files);
 		my @recordBuffer;
-		my $fileTime = time;
+		my $fileTime = time; # start the per file processing timer
 
 		(my $outFile = $inFile) =~ s/$soureDir/$processedDir/;
 
@@ -64,7 +64,10 @@ sub Process_File {
 		close(OUTFILE);
 		close(INFILE);
 
-		printf "%.2f mins (%.2f mins)\n", (time - $start) / 60, (time - $fileTime) / 60;
+		my $fileProcessingTime = (time - $fileTime) / 60;
+		@fileTimes.push($fileProcessingTime);
+
+		printf "%.2f mins (%.2f mins)\n", (time - $start) / 60, $fileProcessingTime;
 	}
 }
 
@@ -76,7 +79,7 @@ sub Process_Record {
 
 	# get the values to be transformed. because the file is fixed width,
 	# this is a manageable way to break out the values
-	my $pan = substr($record, 0, 16); # get pan starting at col 5
+	my $pan = substr($record, 0, 16); # get the card number
 
 	# do the hashing
 	my $hashedPan = sha512_hex($pan . $seed);
@@ -88,4 +91,8 @@ sub Process_Record {
 }
 
 my $duration = (time - $start) / 60;
+my $sum = eval join '+', @fileTimes;
+my $average = $sum / scalar(@fileTimes)
+
 printf "\nProcessing completed in %.2f minutes\n", $duration;
+printf "Average file processing time: %.2f minutes\n", $average;
